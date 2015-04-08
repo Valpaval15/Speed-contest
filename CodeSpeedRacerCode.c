7e0316c35df945f9ab7c42933b98d9cde756375c
Maximilian Litteral <maxhasadhd@gmail.com>
2015-04-08 13:43:40 -0700

    Create CodeSpeedRacerCode.c

---
+++ CodeSpeedRacerCode.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char myString[10];
+    printf("Input a string: \n");
+    scanf("%s", myString);
+    printf("%s\n", myString);
+    printf("%s\n", myString);
+    return 0;
+}
