b9a08134ec9cd46f4788ca602a10b1fb0c5feab7
Maximilian Litteral <maxhasadhd@gmail.com>
2015-04-08 13:47:32 -0700

    Part B

--- CodeSpeedRacerCode.c
+++ CodeSpeedRacerCode.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <string.h>
+int countString(char *string);
 int main(void)
 {
     char myString[10];
@@ -7,5 +8,11 @@ int main(void)
     scanf("%s", myString);
     printf("%s\n", myString);
     printf("%s\n", myString);
+    int count = countString(myString);
+    printf("Count is %i\n", count);
     return 0;
+}
+
+int countString(char *string) {
+    return strlen(string);
 }
